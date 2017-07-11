6649e01e5cdd544202b90f88faa91dbd6df5717a
14120 <>
2017-07-11 22:00:53 +0900

    firstCommit

---
+++ PowershellExecute/frmMain.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace PowershellExecute
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.txtPosh = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.rtbOutput, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtPosh, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(590, 475);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbOutput.Location = new System.Drawing.Point(3, 3);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.ReadOnly = true;
+            this.rtbOutput.Size = new System.Drawing.Size(584, 449);
+            this.rtbOutput.TabIndex = 1;
+            this.rtbOutput.Text = "";
+            // 
+            // txtPosh
+            // 
+            this.txtPosh.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPosh.Location = new System.Drawing.Point(3, 458);
+            this.txtPosh.Name = "txtPosh";
+            this.txtPosh.Size = new System.Drawing.Size(584, 21);
+            this.txtPosh.TabIndex = 0;
+            this.txtPosh.Text = "Type and press Enter.";
+            this.txtPosh.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPosh_KeyPress);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 475);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "frmMain";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.TextBox txtPosh;
+    }
+}
+
---
+++ PowershellExecute/frmMain.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace PowershellExecute
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void txtPosh_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                try
+                {
+                    rtbOutput.Clear();
+                    rtbOutput.AppendText(runScript(txtPosh.Text));
+                }
+                catch (Exception ex)
+                {
+                    rtbOutput.AppendText(ex.Data + Environment.NewLine);
+                }
+                finally
+                {
+                    txtPosh.Clear();
+                }
+            }
+        }
+
+        private string runScript(string s)
+        {
+            Runspace run = RunspaceFactory.CreateRunspace();
+
+            run.Open();
+
+            Pipeline pipe = run.CreatePipeline();
+            pipe.Commands.AddScript(s);
+
+            pipe.Commands.Add("Out-String");
+
+            Collection<PSObject> results = pipe.Invoke();
+
+            run.Close();
+
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (PSObject obj in results)
+            {
+                stringBuilder.AppendLine(obj.ToString());
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            rtbOutput.ReadOnly = true;
+            txtPosh.SelectAll();
+        }
+    }
+}
